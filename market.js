8ce73d2d5a456c447bac80a21ea2e50674736108
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2022-02-05 21:50:38 -0300

    fix

--- market.js
+++ market.js
@@ -4,31 +4,35 @@ document.querySelector('input[type=submit]')
     .addEventListener('click', () => {
         var nomeProduto = document.querySelector('input[name=nome_produto]');
         var precoProduto = document.querySelector('input[name=price]');
+        var quantidadeProduto = document.querySelector('input[name=amount]');
 
         items.push({
             nome: nomeProduto.value,
-            valor: precoProduto.value
+            valor: precoProduto.value,
+            quatidade: quantidadeProduto.value
         });
 
         let listaProdutos = document.querySelector('.lista-produtos');
         let soma = 0;
         listaProdutos.innerHTML = "";
         items.map(function(val) {
-            soma += parseFloat(val.valor);
+            soma += parseFloat(val.valor * val.quatidade);
 
             listaProdutos.innerHTML += `
-        <div class="lista-produto-single">
-            <h3 class="name-produto">` + val.nome + `</h3>
-            <h3 class="price-produto"><span>R$` + val.valor + `</span></h3>
-        </div>
+        <ul class="lista-produto-single">
+            <h3 class="name-produto">` + 'Produto: '+ val.nome + `</h3>
+            <h3 class="amount-produto">` + 'Quantidade: ' + val.quatidade + `</h3>
+            <h3 class="price-produto"><span>R$`+ val.valor + `</span></h3>
+        </ul>
         `;
         })
         soma = soma.toFixed(2);
         nomeProduto.value = "";
         precoProduto.value = "";
+        quantidadeProduto.value = "";
 
         let elementoSoma = document.querySelector('.soma-produto h1');
-        elementoSoma.innerHTML = 'R$' + soma;
+        elementoSoma.innerHTML = 'Total: R$' + soma;
 
 
     });
